983147502f77d2b6d22b1835650012bb252f9378
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-04-25 21:33:35 -0700

    Add project files.

---
+++ COMP003B.Assignment5/Data/PetExample.cs
@@ -0,0 +1,34 @@
+﻿using COMP003B.Assignment5.Models;
+
+namespace COMP003B.Assignment5.Data
+{
+	public class PetExample
+	{
+		public static List<Pets> PetDescription { get; } = new()
+		{
+			new Pets
+			{
+				Id = 4,
+				Name = "Sumo",
+				Type = "Dog",
+				Age = 4
+			},
+
+			new Pets
+			{
+				Id = 3,
+				Name = "Maya",
+				Type = "Cat",
+				Age = 6
+			},
+
+			new Pets
+			{
+				Id = 2,
+				Name = "Charlie",
+				Type = "Hampster",
+				Age = 1
+			}
+		};
+	}
+}
---
+++ COMP003B.Assignment5/Models/Pets.cs
@@ -0,0 +1,13 @@
+﻿namespace COMP003B.Assignment5.Models
+{
+	public class Pets
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Type { get; set; }
+
+		public int Age { get; set; }
+	}
+}
